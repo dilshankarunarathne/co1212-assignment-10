62e9bb07f252a9dc8651283895dffec4b8407d94
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 09:33:25 +0530

    Update Stack.java

--- Solution/src/me/karunarathne/FreakyDataStructures/Stack.java
+++ Solution/src/me/karunarathne/FreakyDataStructures/Stack.java
@@ -1,5 +1,11 @@
 package me.karunarathne.FreakyDataStructures;
 
+import me.karunarathne.NormalDataStructures.CharQueue;
+
 public class Stack {
-    
+    CharQueue queue;
+
+    public Stack() {
+        queue = new CharQueue();
+    }
 }
