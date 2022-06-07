b8bb94e21135689352129c4848b95eb1064aff08
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 09:46:43 +0530

    Update Queue.java

--- Solution/src/me/karunarathne/FreakyDataStructures/Queue.java
+++ Solution/src/me/karunarathne/FreakyDataStructures/Queue.java
@@ -1,5 +1,11 @@
 package me.karunarathne.FreakyDataStructures;
 
+import me.karunarathne.NormalDataStructures.CharStack;
+
 public class Queue {
-    
+    private final CharStack stack;
+
+    public Queue() {
+        stack = new CharStack();
+    }
 }
