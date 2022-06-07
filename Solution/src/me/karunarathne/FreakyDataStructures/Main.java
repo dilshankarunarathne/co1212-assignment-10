246ba5150518ce5c2c867412605cfb9f2dfa9c3a
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 09:40:04 +0530

    Update Main.java

--- Solution/src/me/karunarathne/FreakyDataStructures/Main.java
+++ Solution/src/me/karunarathne/FreakyDataStructures/Main.java
@@ -2,6 +2,7 @@ package me.karunarathne.FreakyDataStructures;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Stack stack = new Stack();
+        stack.push('a');
     }
 }
