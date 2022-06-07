c5823ddd7d0be937509188a2a0b25e10ac2eb080
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 09:55:31 +0530

    Update Queue.java

--- Solution/src/me/karunarathne/FreakyDataStructures/Queue.java
+++ Solution/src/me/karunarathne/FreakyDataStructures/Queue.java
@@ -4,9 +4,11 @@ import me.karunarathne.NormalDataStructures.CharStack;
 
 public class Queue {
     private CharStack stack;
+    private int size;
 
     public Queue() {
         stack = new CharStack();
+        size = 0;
     }
 
     public void enqueue(char c) {
@@ -14,14 +16,16 @@ public class Queue {
         stack = new CharStack();
         stack.push(c);
         copyAllFrom(oldStack);
+        size ++ ;
     }
 
     public Character dequeue() {
-
+        
+        size -- ;
         return null;
     }
 
     private void copyAllFrom(CharStack otherStack) {
-
+        stack.push(otherStack.pop());
     }
 }
